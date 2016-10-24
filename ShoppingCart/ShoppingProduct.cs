660698c0c30b505029d3a8f6a8b42160956269e4
guest0307 <guest0307@GUEST0307-PC>
2016-10-25 02:48:21 +0800

    修復紅燈為綠燈 一二集各買了一本第三集買了兩本價格應為370

--- ShoppingCart/ShoppingProduct.cs
+++ ShoppingCart/ShoppingProduct.cs
@@ -10,22 +10,25 @@ namespace ShoppingCart
     {
         public double CalculateFee(List<Product> products)
         {
-            //Step1:一到五集的哈利波特，每一本都是賣100元
-            //Step2:如果你從這個系列買了兩本不同的書，則會有5%的折扣
             var calculateProducts = products.Where(t => t.Type == ProductEnum.Type.Book);
-            int sum = calculateProducts.Sum(u => u.Price * u.Quantity);
+            int totalQuantity = calculateProducts.Sum(t => t.Quantity);
 
-            int totalEpisode = calculateProducts.Where(t => t.Quantity > 0)
-                                                .Select(u => u.Episode)
-                                                .Distinct()
-                                                .Count();
+            List<int> recordQuantity = new List<int>();
+            int price = 100;
 
-            double[] ruleDiscount = new double[5] {1, 0.95, 0.9, 0.8, 0.75};
+            while (totalQuantity > 0)
+            {
+                calculateProducts.ToList().ForEach(u => u.Quantity = (u.Quantity > 0 ? u.Quantity - 1 : 0));
+                int remainSumQuantity = calculateProducts.Sum(t => t.Quantity);
 
+                int subtractQuantity = totalQuantity - remainSumQuantity;
+                totalQuantity = remainSumQuantity;
+                recordQuantity.Add(subtractQuantity);
+            }
 
-            double discount = ruleDiscount[totalEpisode - 1];
+            double[] ruleDiscount = new double[5] { 1, 0.95, 0.9, 0.8, 0.75 };
 
-            return sum * discount;
+            return recordQuantity.Select(quantity => price * quantity * ruleDiscount[quantity-1]).Sum();
         }
     }
 }
