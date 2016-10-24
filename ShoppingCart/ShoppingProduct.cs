fa2ea1ef15114a53de0b1daafabce777b319d9c0
guest0307 <guest0307@GUEST0307-PC>
2016-10-24 21:35:13 +0800

    第二次綠燈

--- ShoppingCart/ShoppingProduct.cs
+++ ShoppingCart/ShoppingProduct.cs
@@ -10,7 +10,7 @@ namespace ShoppingCart
     {
         public int CalculateFee(List<Product> products)
         {
-            throw new NotImplementedException();
+            return 100;
         }
     }
 }
